2fb6136ddb0b844909d134f2f57d06ef5513c8aa
jv <ljw@live.jp>
2019-04-13 18:48:08 +0800

    feat: improve argv

--- run.js
+++ run.js
@@ -2,19 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const eggScript = path.resolve('node_modules/egg-scripts/bin/egg-scripts.js');
 const packageJson = path.resolve('package.json');
-const builtIn = path.resolve('node_modules/eggless/built-in');
+const baseUrl = path.resolve('node_modules/eggless/built-in');
 
 // copy package.json
-fs.copyFileSync(packageJson, builtIn.concat('/package.json'));
+fs.copyFileSync(packageJson, baseUrl.concat('/package.json'));
 
 // refactor argv
-const [, ...rawOptions] = process.argv.slice(2);
-rawOptions.forEach((option, index) => {
-  if (!/^--/.test(option)) {
-    process.argv.splice(index + 3, 1);
-  }
-});
-process.argv.push(builtIn);
+const [node, script, command, ...rawOptions] = process.argv;
+process.argv = [node, script, command, ...rawOptions.filter(option => /^--/.test(option))];
+
+// push baseUrl
+process.argv.push(baseUrl);
 
 // next
 require(eggScript);
