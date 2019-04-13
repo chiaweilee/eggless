42477a73808a5ca9094ff571a9ce402e67211d2f
jv <ljw@live.jp>
2019-04-13 17:33:05 +0800

    feat: copy package.json

--- run.js
+++ run.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
 const path = require('path');
 const eggScript = path.resolve('node_modules/egg-scripts/bin/egg-scripts.js');
+const packageJson = path.resolve('package.json');
 const builtIn = path.resolve('node_modules/eggless/built-in');
+
+// copy package.json
+fs.copyFileSync(packageJson, builtIn.concat('/package.json'));
 
 // refactor argv
 const [, ...rawOptions] = process.argv.slice(2);
